e2f34480b924578c7d2ffb8bd17643a72a5f0604
DigoAmorim <52294374+DigoAmorim@users.noreply.github.com>
2021-01-07 08:18:55 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const path = require('path');
+
+const port = process.env.PORT || 8000;
+
+function getDbPath(filename) {
+  return path.resolve(__dirname, `data/${filename}`);
+}
+
+const dbPath = getDbPath('db.json');
+
+if (!fs.existsSync(dbPath)) {
+  console.log('Initializing database');
+  fs.copyFileSync(getDbPath('db.init.json'), dbPath);
+}
+
+// server.js
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(dbPath);
+const middlewares = jsonServer.defaults();
+
+router.render = (req, res) => {
+  setTimeout(function sendResponse() {
+    res.jsonp(res.locals.data);
+  }, 500 + Math.round(500 * Math.random()));
+}
+
+server.use(middlewares);
+server.use(router);
+server.listen(port, () => {
+  console.log(`Servidor executando na porta ==> ${port}...`);
+});
